d10e1518ee6135b0ab0dce611157d2b74ffdf3f6
Shriyanshu Raj <rajshriyanshu5@gmail.com>
2025-04-13 09:46:13 +0530

    build error

--- utils/index.ts
+++ utils/index.ts
@@ -1,28 +1,27 @@
-'use client';
+"use client";
 
 import { User } from "@/dto";
 
 export function convertTimestampToDate(timestamp: string): string {
-    const date = new Date(timestamp);
-    const day = String(date.getDate()).padStart(2, "0");
-    const month = String(date.getMonth() + 1).padStart(2, "0");
-    const year = date.getFullYear();
-    return `${day}/${month}/${year}`;
-  }
+  const date = new Date(timestamp);
+  const day = String(date.getDate()).padStart(2, "0");
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const year = date.getFullYear();
+  return `${day}/${month}/${year}`;
+}
 
 export const getStoredUser = () => {
-  if(window === undefined)return null;
+  if (typeof window === "undefined") return null;
   const stored = localStorage.getItem("user");
   return stored ? JSON.parse(stored) : null;
 };
 
 export const setStoredUser = (user: User) => {
-  if(window === undefined)return null;
-
-    localStorage.setItem("user", JSON.stringify(user));
+  if (typeof window === "undefined") return null;
+  localStorage.setItem("user", JSON.stringify(user));
 };
 
 export const clearStoredUser = () => {
-  if(window === undefined)return;
-    localStorage.removeItem("user");
+  if (typeof window === "undefined") return null;
+  localStorage.removeItem("user");
 };
